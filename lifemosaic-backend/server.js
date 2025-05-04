c9dda6352168eec10ff21a936fb3fcd9ce00171e
JOY MUKHOPADHYAY <joyba1406@gmail.com>
2025-05-04 09:29:02 +0000

    save

--- lifemosaic-backend/server.js
+++ lifemosaic-backend/server.js
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // MongoDB Connection
-mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.DB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.log('MongoDB connection error:', err));
 
